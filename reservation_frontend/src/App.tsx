8f864b438bf2383408ff9b621c951663518ca2a6
Connor Bowling <46806071+ConnorB26@users.noreply.github.com>
2024-01-03 18:15:13 -0600

    Updated public files

--- reservation_frontend/src/App.tsx
+++ reservation_frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import NotFoundPage from './pages/NotFoundPage';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import BlankPage from './pages/BlankPage';
 import LoginPage from './pages/LoginPage';
 import withAuth from './utilities/withAuth';
 import { AuthProvider } from './utilities/AuthContext';
 
-// Wrapped components
 const AuthBlankPage = withAuth(BlankPage, "default");
 const AuthReservationPage = withAuth(BlankPage, "reservation");
 const AuthAdminPage = withAuth(BlankPage, "admin");
@@ -16,10 +14,10 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route index element={<AuthBlankPage />} />
+          <Route path="login" element={<LoginPage />} />
           <Route path="reservation" element={<AuthReservationPage />} />
           <Route path="admin" element={<AuthAdminPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
